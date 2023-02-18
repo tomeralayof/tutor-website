266904ee2b72d01740b37b3eb9cda9d89a0bc9b0
tomeralayof <tomeralayof95959@gmail.com>
2023-02-18 18:29:06 +0200

    update responsive text

--- src/pages/welcomePage.jsx
+++ src/pages/welcomePage.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Header } from "../common/header";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 export const WelcomePage = () => {
     return (
         <React.Fragment>
             <Header text = {" { ללמוד תכנות בקלות }"} />
             <p>שיעורים פרטיים בתכנות במגוון שפות ולכל מטרה</p>
-{/*             <img src="https://dynamic.brandcrowd.com/preview/logodraft/2ed14ea2-c467-4cc4-9f46-119b71d77963/image/large.png"/>
- */}        </React.Fragment>
+
+            <div className="nav-instraction-container">
+            <div className = "content-container">          
+                <p className = "paragraph">
+                    <FontAwesomeIcon icon = {faArrowLeft} />
+                        <span className = "paragraph__text">לניווט מהיר</span>
+                    <FontAwesomeIcon icon={faArrowRight} />
+                </p>
+            </div>
+            </div>
+
+        </React.Fragment>
     )
 }
--- src/sections/section.jsx
+++ src/sections/section.jsx
@@ -3,10 +3,9 @@ import React from "react";
 import "../styles/Section/section.css"
 
 export const Section = ({ page }) => (
-  <section className={page.cssClass} id={`st-panel-${page.id}`}>
+    <section className={page.cssClass} id={`st-panel-${page.id}`}>
       <div className="st-deco" data-icon = "&#xf069;" ></div>
       {page.component}
     </section>
 );
 
-{/* <img src = "https://publicdomainvectors.org/photos/nicubunu_Comic_characters_Operator.png"/> */}
