67add792a11f0758aea22528fc491a097640f79b
tomeralayof <tomeralayof95959@gmail.com>
2023-03-01 00:14:55 +0200

    update

--- src/components/contact/form/Input.jsx
+++ src/components/contact/form/Input.jsx
@@ -3,28 +3,23 @@ import React , { useContext, useEffect,useState , useCallback } from "react";
 import keyboardContext from "../../../hooks/keyboardContext/createContext";
 
 export const Input = ({ filed ,handleInputChange,idx }) => {
-
   const isMobileDevice = /Mobi|Android/i.test(navigator.userAgent);
   const { setIsKeyboardUp } = useContext(keyboardContext);
-  const [isKeyboardOpen] = useState(false);
-  
-  const handleResize = useCallback(() => {
-    console.log("triggered ...");
-    setIsKeyboardUp(isKeyboardOpen ? true : false);
-  },[setIsKeyboardUp,isKeyboardOpen]);
 
-  useEffect(() => {
-    if (window.innerHeight < window.outerHeight) {
-      window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+  const handleInputTrack = (hasToCloseButtomElements) => {
+    if(isMobileDevice) {
+      setIsKeyboardUp(hasToCloseButtomElements);
     }
-  },[handleResize,isMobileDevice]);
-  
-  console.log("is mobile = ",isMobileDevice);
+  }
 
+  const handleInputFocus = () => {
+    handleInputTrack(true);
+  };
+
+  const handleInputBlur = () => {
+    handleInputTrack(false);
+  };
+  
   return (
       <input
       onChange = {(e) => handleInputChange(e,idx)}
@@ -34,54 +29,8 @@ export const Input = ({ filed ,handleInputChange,idx }) => {
       style = {{ direction: "rtl" }}
       autoComplete = "given-name"
       name = { filed.filedName }
+      onFocus = {handleInputFocus}
+      onBlur = {handleInputBlur}
     />
   )
 }
-
-
-
-/* 
-import React, { useState, useEffect } from "react";
-
-function App() {
-  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
-
-  useEffect(() => {
-    function handleResize() {
-      const keyboardOpen = window.innerHeight < window.outerHeight;
-      setIsKeyboardOpen(keyboardOpen);
-
-      // Call your function here every time the keyboard is opened or closed
-      myFunction(keyboardOpen);
-    }
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  function myFunction(keyboardOpen) {
-    if (keyboardOpen) {
-      console.log("Keyboard opened");
-      // Do something when the keyboard is opened
-    } else {
-      console.log("Keyboard closed");
-      // Do something when the keyboard is closed
-    }
-  }
-
-  return (
-    <div>
-      {isKeyboardOpen ? <p>Keyboard is open</p> : <p>Keyboard is closed</p>}
-    </div>
-  );
-}
-
-
-
-
-
-
-*/
