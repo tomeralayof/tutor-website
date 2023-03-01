84a874af070adf13523c9d529475a6f3bb0c6579
tomeralayof <tomeralayof95959@gmail.com>
2023-03-01 20:00:44 +0200

    update photos

--- src/config/courses/imageData.js
+++ src/config/courses/imageData.js
@@ -1,7 +1,7 @@
 export const courseData = [
     {
         name : "Java Script",
-        url : "https://miro.medium.com/max/800/1*M_GDn6RXZSZC66kvrMHh5Q.png",
+        url : "https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_960_720.png",
         style : {
             backgroundColor : "#F0DB4F",
         }
@@ -9,7 +9,7 @@ export const courseData = [
     },
     {
         name : "Python",
-        url : "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/9d2e8896750899.5eb54f3381452.png",
+        url : "https://images.unsplash.com/photo-1649180556628-9ba704115795?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1462&q=80",
         style : {
             background: "linear-gradient(to right bottom, #f11964 20%, #f57639 60%, #f7a920 100%)",
             color : "black"
@@ -17,7 +17,7 @@ export const courseData = [
     },
     {
         name : "C++",
-        url : "https://www.educative.io/v2api/editorpage/5393602882568192/image/6038586442907648",
+        url : "https://cdn.pixabay.com/photo/2015/02/03/12/23/c-622435_960_720.jpg",
         style : {
             background: "linear-gradient(to right bottom, silver 10%, teal 90% 100%)",
             color : "black"
@@ -26,7 +26,7 @@ export const courseData = [
 
     {
         name : "Node Js",
-        url : "https://informatics.uii.ac.id/wp-content/uploads/2022/09/nodejs.png",
+        url : "https://cdn.pixabay.com/photo/2015/04/23/17/41/node-js-736399_960_720.png",
         style : {
             background: "linear-gradient(to right bottom, #3c873a 20%, #68a063 60% 100%)",
         }
@@ -36,13 +36,13 @@ export const courseData = [
         name : "React",
         url : "https://reactjs.org/logo-og.png",
         style : {
-            background: "linear-gradient(to right bottom, black 5%, #61DBFB 95% 100%)",
+            background: "linear-gradient(to right bottom, #61DBFB 95% 100%)",
         }
     },
 
     {
         name : "Data Structures",
-        url : "https://i.ytimg.com/vi/Qmt0QwzEmh0/maxresdefault.jpg",
+        url : "https://cdn.pixabay.com/photo/2014/11/04/08/07/mark-516278_960_720.jpg",
         style : {
             background: "black",
             color : "white"
@@ -60,7 +60,7 @@ export const courseData = [
 
     {
         name : "C languge",
-        url : "https://1.bp.blogspot.com/-KnGVLo7U7gQ/Xx0E8Sh8KtI/AAAAAAAAhnE/THS8q3fq9zg3jWTQqFr2s0XuE_M4BABNQCLcBGAsYHQ/w1200-h630-p-k-no-nu/free%2BC%2BProgramming%2Bcourses%2Bfor%2Bbeginners.png",
+        url : "https://images.unsplash.com/photo-1527957557037-d079c24f24be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
         style : {
             backgroundColor : "#baeeff",
             color : "black"
