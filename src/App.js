6212095614e8b1b935e60ea83e02f771b29e6f29
tomeralayof <tomeralayof95959@gmail.com>
2023-03-01 15:55:42 +0200

    remove loading for now

--- src/App.js
+++ src/App.js
@@ -1,52 +1,28 @@
-import React, { useState ,useContext,useEffect, useRef } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+
 import "./App.css"
 
 import { NavBar } from './components/nabBar/navBar';
 import { Sections } from "./sections/sections";
 
-import LoadingPage from './loading/loadingPage';
-
 import keyboardContext from "./hooks/keyboardContext/createContext";
 
 const App = () => {
   const { isKeyboardUp } = useContext(keyboardContext);
-  const [section,setSection] = useState(5);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  const hasLoaded = useRef(false);
+  const [section, setSection] = useState(5);
 
   useEffect(() => {
     setSection(1);
-  },[isKeyboardUp]);
-
-  useEffect(() => {
-    if (hasLoaded.current) {
-      setIsLoaded(true);
-    } else {
-      const handleLoad = () => {
-        hasLoaded.current = true;
-        setIsLoaded(true);
-      };
-  
-      window.addEventListener('load', handleLoad);
-  
-      return () => {
-        window.removeEventListener('load', handleLoad);
-      };
-    }
-  }, []);
+  }, [isKeyboardUp]);
 
   return (
     <React.Fragment>
-      {isLoaded ? (
-      <div className= "container">
-        <div className = "st-container">
-        { !isKeyboardUp ?  <NavBar section = { section } /> : null }
-        <Sections />
+        <div className="container">
+          <div className="st-container">
+            {!isKeyboardUp ? <NavBar section={section} /> : null}
+            <Sections />
+          </div>
         </div>
-      </div>
-      ) : <LoadingPage/> }
-
     </React.Fragment>
   );
 }
