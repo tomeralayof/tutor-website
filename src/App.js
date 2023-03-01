5892c919df5c7729a86a42bcab7ba0d0f75f1d3c
tomeralayof <tomeralayof95959@gmail.com>
2023-03-01 15:41:10 +0200

    test loading page in prod

--- src/App.js
+++ src/App.js
@@ -4,24 +4,44 @@ import "./App.css"
 import { NavBar } from './components/nabBar/navBar';
 import { Sections } from "./sections/sections";
 
+import LoadingPage from './loading/loadingPage';
+
+
 import keyboardContext from "./hooks/keyboardContext/createContext";
 
 const App = () => {
   const { isKeyboardUp } = useContext(keyboardContext);
   const [section,setSection] = useState(5);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     setSection(1);
   },[isKeyboardUp]);
 
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsLoaded(true);
+    };
+
+    window.addEventListener('load', handleLoad);
+
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
+  }, []);
+
   return (
     <React.Fragment>
+      {isLoaded ? (
       <div className= "container">
         <div className = "st-container">
         { !isKeyboardUp ?  <NavBar section = { section } /> : null }
         <Sections />
         </div>
       </div>
+      ) : <LoadingPage/> }
+
     </React.Fragment>
   );
 }
---
+++ src/loading/loadingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoadingPage = () => {
+  return (
+    <div>
+      <h1>Loading...</h1>
+      {/* Add your loading spinner or other content here */}
+    </div>
+  );
+};
+
+export default LoadingPage;
